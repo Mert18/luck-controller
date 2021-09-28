5c656d4cfbf5fd0dda8893f5c2ba8287c7cf2f3e
mert18 <mertuygur02@gmail.com>
2021-09-28 18:03:24 +0300

    floating bubbles renewed, color scheme changed.

--- src/screens/FloatingBubbles.tsx
+++ src/screens/FloatingBubbles.tsx
@@ -1,122 +1,78 @@
-import React, { useState } from "react";
+import React, { ButtonHTMLAttributes, useEffect, useState } from "react";
 import Layout from "../components/Layout";
 import "../styles/floatingBubbles.css";
 
 const FloatingBubbles = () => {
-  const [left, setLeft] = useState([
-    50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50,
-  ]);
-  const [top, setTop] = useState(
-    Array.from({ length: 11 }, () => Math.floor(Math.random() * 80))
-  );
+  const [random, setRandom] = useState<number[]>([50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50])
 
-  const [leftBack, setLeftBack] = useState("");
-  const [rightBack, setRightBack] = useState("");
+  const [result, setResult] = useState("");
+  const [myArr, setMyArr] = useState<number[]>([]);
+  const [history, setHistory] = useState<boolean[]>([])
+  const [chosen, setChosen] = useState("")
+  const gameStartForDown = () => {
+      setRandom(Array.from({length: 11}, () => Math.floor(Math.random() * 100))); 
+      setMyArr(random.filter((el) => el < 50))
+    setChosen("down")
+  }
 
-  const leftHandler = () => {
-    setLeft(Array.from({ length: 11 }, () => Math.floor(Math.random() * 100)));
-    setTop(Array.from({ length: 11 }, () => Math.floor(Math.random() * 80)));
-    setTimeout(() => {
-      if (left.filter((el) => el < 50).length > 5.5) {
-        setLeftBack("rgba(75, 250, 75, 0.63)");
-      } else {
-        setLeftBack("rgba(255, 0, 0, 0.246)");
-      }
-    }, 1000);
-    setTimeout(() => {
-      setLeftBack("transparent");
-      setRightBack("transparent");
-
-      // setLeft([50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50]);
-      console.log(left.filter((el) => el < 50));
-    }, 2000);
-  };
-
-  const rightHandler = () => {
-    setLeft(Array.from({ length: 11 }, () => Math.floor(Math.random() * 100)));
-    setTop(Array.from({ length: 11 }, () => Math.floor(Math.random() * 80)));
-    setTimeout(() => {
-      if (left.filter((el) => el > 50).length > 5.5) {
-        setRightBack("rgba(75, 250, 75, 0.63)");
-      } else {
-        setRightBack("rgba(255, 0, 0, 0.246)");
-      }
-    }, 1000);
-    setTimeout(() => {
-      setRightBack("transparent");
-      setLeftBack("transparent");
-      // setLeft([50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50]);
-    }, 2000);
-  };
-
-  return (
+  const gameStartForUp = () => {
+   setRandom(Array.from({length: 11}, () => Math.floor(Math.random() * 100))); 
+    setMyArr(random.filter((el) => el > 50))
+    setChosen("up")
+  }
+  useEffect(() => {
+    if(myArr.length >= 6 && chosen === "down" || myArr.length >=6 && chosen === "up"){
+      setHistory([...history, true])
+    }
+    else{
+      setHistory([...history, false]);
+    }
+  }, [myArr])
+  return(
     <Layout>
       <section className="floating-bubbles">
         <div className="gamescreen">
-          <div className="floating">
+          <div className="floating-head">
             <h2>Floating Bubbles ( 50% )</h2>
-            <p>Pick one side.</p>
-            <button
-              style={{ background: `${leftBack}` }}
-              className="leftsec"
-              onClick={leftHandler}
-            ></button>
-            <button
-              style={{ background: `${rightBack}` }}
-              className="rightsec"
-              onClick={rightHandler}
-            ></button>
+            <p>Every box will change to a number between 0 - 100. Will they mostly be lower than 50 or higher?</p>
+            <div className="floating-choose">
+              <button className="floating-down" onClick={() => {
+                  gameStartForDown();
+                }}>
+                <p>LOWER</p>
+              </button>
+              <button className="floating-up" onClick={() => {
+                    gameStartForUp();
+                }}>
+                <p>HIGHER</p>
+              </button>
+            </div>
+          </div>
+          <div className="floating-game">
             <div className="bubbles">
-              <div
-                className="bubble"
-                style={{ left: `${left[0]}%`, top: `${top[0]}%` }}
-              ></div>
-              <div
-                className="bubble"
-                style={{ left: `${left[1]}%`, top: `${top[1]}%` }}
-              ></div>
-              <div
-                className="bubble"
-                style={{ left: `${left[2]}%`, top: `${top[2]}%` }}
-              ></div>
-              <div
-                className="bubble"
-                style={{ left: `${left[3]}%`, top: `${top[3]}%` }}
-              ></div>
-              <div
-                className="bubble"
-                style={{ left: `${left[4]}%`, top: `${top[4]}%` }}
-              ></div>
-              <div
-                className="bubble"
-                style={{ left: `${left[5]}%`, top: `${top[5]}%` }}
-              ></div>
-              <div
-                className="bubble"
-                style={{ left: `${left[6]}%`, top: `${top[6]}%` }}
-              ></div>
-              <div
-                className="bubble"
-                style={{ left: `${left[7]}%`, top: `${top[7]}%` }}
-              ></div>
-              <div
-                className="bubble"
-                style={{ left: `${left[8]}%`, top: `${top[8]}%` }}
-              ></div>
-              <div
-                className="bubble"
-                style={{ left: `${left[9]}%`, top: `${top[9]}%` }}
-              ></div>
-              <div
-                className="bubble"
-                style={{ left: `${left[10]}%`, top: `${top[10]}%` }}
-              ></div>
+              {random.map((el, i) => (<div className="bubble" key={i}>
+                {el}
+              </div>))}
+           </div>
+            <div className="floating-result">
+              {myArr.map((el, i) => (<div className="bubble" key={i}>
+                {el}
+              </div>))}
             </div>
           </div>
         </div>
+        <div className="truefalse">
+          {history.map((el) => {
+            if(el === true){
+              return <div className="true"></div>
+            }else{
+              return <div className="false"></div>
+            }
+          })}
+        </div>
       </section>
     </Layout>
-  );
+  )
 };
 
 export default FloatingBubbles;
